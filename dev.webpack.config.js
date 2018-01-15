112e2b5ab67ae07e56be32f004f6bc8935692a54
Alejandro Frías <alejandrofdiaz@gmail.com>
2018-01-15 22:06:39 +0100

    added prod variables

--- dev.webpack.config.js
+++ dev.webpack.config.js
@@ -1,70 +1,70 @@
-const
-	webpack = require('webpack'),
-	webpackMerge = require('webpack-merge'),
-	ExtractTextPlugin = require('extract-text-webpack-plugin'),
-	commonConfig = require('./base.webpack.config.js');
+const webpack = require('webpack'),
+  webpackMerge = require('webpack-merge'),
+  ExtractTextPlugin = require('extract-text-webpack-plugin'),
+  commonConfig = require('./base.webpack.config.js');
 
 module.exports = () =>
-	webpackMerge(commonConfig, {
-		module: {
-			rules: [
-				{
-					test: /\.css$/,
-					loader: ExtractTextPlugin.extract({
-						fallback: 'style-loader',
-						use: [
-							{
-								loader: 'css-loader',
-								options: {
-									modules: false,
-									sourceMap: true
-								}
-							},
-							{
-								loader: 'postcss-loader',
-								options: {
-									sourceMap: true
-								}
-							}
-						]
-					})
-				},
-				{
-					test: /\.(scss|sass)$/,
-					loader: ExtractTextPlugin.extract({
-						fallback: 'style-loader',
-						use: [
-							{
-								loader: 'css-loader',
-								options: {
-									modules: false,
-									sourceMap: true
-								}
-							},
-							{
-								loader: 'postcss-loader',
-								options: {
-									sourceMap: true
-								}
-							},
-							{
-								loader: 'sass-loader',
-								options: {
-									sourceMap: true
-								}
-							}
-						]
-					})
-				},
-			]
-		},
-		devtool: 'inline-source-map',
-		plugins: [
-			new webpack.DefinePlugin({
-				'process.env': {
-					'GOOGLE_API_KEY': JSON.stringify('AIzaSyDbtEXX5KAQWgVfRUoDFj5BOkPcUHt8j2w'),
-					'GOOGLE_CAPTCHA_KEY': JSON.stringify('6LcXNTMUAAAAAF6VkfDnlA-K4RAPsg6e7vs_iWsg')
-				}
-			}),
-		]
-	});
+  webpackMerge(commonConfig, {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: false,
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+        },
+        {
+          test: /\.(scss|sass)$/,
+          loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: false,
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+        }
+      ]
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('development'),
+          GOOGLE_API_KEY: JSON.stringify('AIzaSyDbtEXX5KAQWgVfRUoDFj5BOkPcUHt8j2w'),
+          GOOGLE_CAPTCHA_KEY: JSON.stringify('6LcXNTMUAAAAAF6VkfDnlA-K4RAPsg6e7vs_iWsg')
+        }
+      })
+    ]
+  });
--- prod.webpack.config.js
+++ prod.webpack.config.js
@@ -1,77 +1,80 @@
-const
-	webpack = require('webpack'),
-	webpackMerge = require('webpack-merge'),
-	ExtractTextPlugin = require('extract-text-webpack-plugin'),
-	commonConfig = require('./base.webpack.config.js'),
-	UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack'),
+  webpackMerge = require('webpack-merge'),
+  ExtractTextPlugin = require('extract-text-webpack-plugin'),
+  commonConfig = require('./base.webpack.config.js'),
+  UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = () =>
-	webpackMerge(commonConfig, {
-		module: {
-			rules: [
-				{
-					test: /\.css$/,
-					loader: ExtractTextPlugin.extract({
-						fallback: 'style-loader',
-						use: [
-							{
-								loader: 'css-loader',
-								options: {
-									minimize: true,
-									modules: false,
-									sourceMap: false
-								}
-							},
-							{
-								loader: 'postcss-loader'
-							}
-						]
-					})
-				},
-				{
-					test: /\.(scss|sass)$/,
-					loader: ExtractTextPlugin.extract({
-						fallback: 'style-loader',
-						use: [
-							{
-								loader: 'css-loader',
-								options: {
-									minimize: true,
-									modules: false,
-									sourceMap: false
-								}
-							},
-							{
-								loader: 'postcss-loader'
-							},
-							{
-								loader: 'sass-loader',
-								options: {
-									sourceMap: false
-								}
-							}
-						]
-					})
-				},
-			]
-		},
-		devtool: 'none',
-		plugins: [
-			new webpack.DefinePlugin({
-				'process.env': {
-					'GOOGLE_API_KEY': JSON.stringify('AIzaSyDbtEXX5KAQWgVfRUoDFj5BOkPcUHt8j2w')
-				}
-			}),
-			new UglifyJSPlugin({
-				sourceMap: false,
+  webpackMerge(commonConfig, {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  minimize: true,
+                  modules: false,
+                  sourceMap: false
+                }
+              },
+              {
+                loader: 'postcss-loader'
+              }
+            ]
+          })
+        },
+        {
+          test: /\.(scss|sass)$/,
+          loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  minimize: true,
+                  modules: false,
+                  sourceMap: false
+                }
+              },
+              {
+                loader: 'postcss-loader'
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: false
+                }
+              }
+            ]
+          })
+        }
+      ]
+    },
+    devtool: 'none',
+    plugins: [
+      new webpack.DefinePlugin({
+        __REACT_DEVTOOLS_GLOBAL_HOOK__: JSON.stringify(false),
+        'process.env': {
+          NODE_ENV: JSON.stringify('production'),
 
-				uglifyOptions: {
-					output: {
-						comments: false,
-						beautify: false,
-						quote_style: 1
-					}
-				},
-			})
-		]
-	});
+          GOOGLE_API_KEY: JSON.stringify('AIzaSyDbtEXX5KAQWgVfRUoDFj5BOkPcUHt8j2w'),
+          GOOGLE_CAPTCHA_KEY: JSON.stringify('6LcXNTMUAAAAAF6VkfDnlA-K4RAPsg6e7vs_iWsg')
+        }
+      }),
+      new UglifyJSPlugin({
+        sourceMap: false,
+
+        uglifyOptions: {
+          output: {
+            comments: false,
+            beautify: false,
+            quote_style: 1
+          }
+        }
+      })
+    ]
+  });
